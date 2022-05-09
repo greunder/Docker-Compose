e3a5fbcffa32e3f96cbed950478c0a43f179e9ab
greunder <82394502+greunder@users.noreply.github.com>
2022-05-09 17:57:52 +0200

    Update process.py

--- process/process.py
+++ process/process.py
@@ -21,10 +21,3 @@ model.fit(x, y)
 # predict y from the data
 x_new = np.linspace(0, 30, 100)
 y_new = model.predict(x_new[:, np.newaxis])
-# create a linear regression model
-model = LinearRegression()
-model.fit(x, y)
-
-# predict y from the data
-x_new = np.linspace(0, 30, 100)
-y_new = model.predict(x_new[:, np.newaxis])
